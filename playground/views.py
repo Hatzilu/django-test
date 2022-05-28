0c4353d2f1609577583e2106a5868bc42ce01569
Kapeet <itamar246@gmail.com>
2022-05-28 11:01:56 +0000

    added view

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def sayHello(request): 
 
-    return HttpResponse('Hello World!')
+def calculate(): 
+    x = 1
+    y = 2
+    return x + y
+
+
+def sayHello(request): 
+    x = calculate()
+    print(request)
+    return render(request, 'hello.html', { 'request': request})
